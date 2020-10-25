b094e7dc1a3c918cf981a96f70650567dcf0f506
Alfian Sanjaya <alfian.sanjaya2000@gmail.com>
2020-10-25 15:21:36 +0100

    Added 2 buttons in MainActiviy
    Buttons will redirect to different activities in the app

--- app/src/main/java/com/example/mobiledevelopment/MainActivity.java
+++ app/src/main/java/com/example/mobiledevelopment/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.mobiledevelopment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +14,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("test", "testje0");
+    }
+
+    public void LaunchSecondActivity(View view) {
+        Intent intent = new Intent(this, Settings.class);
+        startActivity(intent);
+        // Get text from EditText as a string
+        //String message = mMessageEditText.getText().toString();
+        // Add that string to the Intent as an extra with the EXTRA_MESSAGE constant as the key and the string as the value
+        //intent.putExtra(EXTRA_MESSAGE, message);
+        //startActivityForResult(intent);
+    }
+
+    public void LaunchBibActivity(View view) {
+        Intent intent = new Intent(this, Bib.class);
+        startActivity(intent);
     }
 }
