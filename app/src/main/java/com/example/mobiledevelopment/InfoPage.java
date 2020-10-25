6fd0072714e50d27b7fbc86802f53e5b48539ee2
Alfian Sanjaya <alfian.sanjaya2000@gmail.com>
2020-10-25 14:45:38 +0100

    Added 3 activities in app
    Bib, Info -and settings activity

--- app/src/main/java/com/example/mobiledevelopment/InfoPage.java
+++ app/src/main/java/com/example/mobiledevelopment/InfoPage.java
@@ -3,14 +3,12 @@ package com.example.mobiledevelopment;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+public class InfoPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Log.d("test", "testje0");
+        setContentView(R.layout.activity_info_page);
     }
 }
